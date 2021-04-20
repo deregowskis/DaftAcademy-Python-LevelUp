d20bdb855147e6cd94beea2df806a920d1d0552b
Sebastian Deręgowski <56119394+deregowskis@users.noreply.github.com>
2021-04-20 23:00:42 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
 
 
 @app.get("/method")
-def getmethod(request):
+def getmethod(request: Request):
     if request.method == 'GET':
         return {"method": "GET"}
 
