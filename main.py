0aaf94dc4784a7a11936761b42ad2b37d62cd4ff
Sebastian Deręgowski <56119394+deregowskis@users.noreply.github.com>
2021-05-11 22:21:19 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
-from fastapi import FastAPI
-from fastapi import Request
+from fastapi import FastAPI, Response, status, HTTPException
+from typing import NoReturn, Optional
+from pydantic import BaseModel
+import sqlite3
+import sys
 
 app = FastAPI()
 
+class Category(BaseModel):
+    name: str
+
+
+@app.on_event("startup")
+async def startup():
+    app.db_connection = sqlite3.connect("northwind.db")
+    app.db_connection.text_factory = lambda b: b.decode(errors="ignore")
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    app.db_connection.close()
+
+
 @app.get("/")
-def root():
-    return {"message": "Hello world!"}
+async def main():
+    return {"sqlite3_library_version": sqlite3.version, "sqlite_version": sqlite3.sqlite_version}
 
 
-@app.get("/method")
-def root(request: Request):
-    return {"method": f"{request.method}"}
+@app.get("/categories")
+async def categories():
+    categories = app.db_connection.execute('''SELECT CategoryID AS id, CategoryName AS name
+                                              FROM Categories
+                                              ORDER BY id
+                                              ''')
+    return {"categories": [{"id": row[0], "name": row[1]} for row in categories]}
 
-@app.put("/method")
-def root(request: Request):
-    return {"method": f"{request.method}"}
 
-@app.post("/method",status_code=201)
-def root(request: Request):
-    return {"method": f"{request.method}"}
-
-@app.delete("/method")
-def root(request: Request):
-    return {"method": f"{request.method}"}
-
-@app.options("/method")
-def root(request: Request):
-    return {"method": f"{request.method}"}
-
+@app.get("/customers")
+async def customers():
+    customers = app.db_connection.execute('''Select CustomerID AS id, CompanyName AS name, COALESCE(Address,''), COALESCE(PostalCode,''), COALESCE(City,''), COALESCE(Country,'')
+                                             FROM Customers
+                                             ORDER BY id COLLATE NOCASE
+                                             ''')
+    return {"customers": [{"id": row[0], "name": row[1], "full_address": "{} {} {} {}".format(row[2], row[3], row[4], row[5])} for row in customers]}
 
